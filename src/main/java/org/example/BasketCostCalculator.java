fcbebf00574a09e741c79f649ed7fabe2a425478
Naveen <naveen.dhankher@gmail.com>
2021-01-25 02:19:02 +0000

    [Naveen] - Adding few more logs

--- src/main/java/org/example/BasketCostCalculator.java
+++ src/main/java/org/example/BasketCostCalculator.java
@@ -8,21 +8,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class BasketCostCalculator {
 
     private final ItemService itemService;
+    private final static Logger LOG = Logger.getLogger(BasketCostCalculator.class.getName());
 
     BasketCostCalculator(ItemService itemService) {
         this.itemService = itemService;
     }
 
     public Double calculateBasketCost(Map<String, Integer> itemsInCart, PromotionalRules promotionalRules) {
+        LOG.info("Creating Basket details from cart items");
         BasketDetails basketDetails = mapPriceToItemsInCart(itemsInCart);
 
         if (Objects.isNull(promotionalRules) || Objects.isNull(promotionalRules.getPromotionRules()) || promotionalRules.getPromotionRules().isEmpty())
             return getTotalCostForItemsInCart(basketDetails.getBasketItems());
+
+        LOG.info("Applying promotion rules to cart items");
 
         promotionalRules
                 .getPromotionRules()
